6b7b5ef0de27661364b9e8a72c390b74b6d29c7d
Poltorak Semyon <epsilond1@yandex.ru>
2017-12-04 22:18:18 +0500

    Парсинг lenta.ru

---
+++ sem5-lab2/sem5-lab2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace sem5_lab2
+{
+    class Program
+    {
+        static string html = @"https://lenta.ru/";
+        static string top_news = "//*[@id='root']/section[2]/div/div/div[2]/div[1]/section[1]/div/div[{0}]/a/text()";
+        static string general_news = "//*[@id='root']/section[2]/div/div/div[1]/section[1]/div[2]/div[1]/div[{0}]/a/text()";
+
+        static string normalize_string(string str)
+        {
+            return str.Replace("&nbsp;", " ");
+        }
+
+
+        static void print(int start, int count, string pattern, HtmlDocument _doc, string msg)
+        {
+            Console.WriteLine(msg);
+
+            for (int index = start; index < count; ++index)
+            {
+                var response = _doc.DocumentNode.SelectNodes(string.Format(pattern, index));
+                Console.WriteLine(normalize_string(response[0].OuterHtml));
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+
+            var _doc = web.Load(html);
+
+            int count_top_news = _doc.DocumentNode.SelectNodes("//*[@id='root']/section[2]/div/div/div[2]/div[1]/section[1]/div/div").Count;
+            print(2, count_top_news, top_news, _doc, "Print Achtung News");
+
+            int count_general_news = _doc.DocumentNode.SelectNodes("//*[@id='root']/section[2]/div/div/div[1]/section[1]/div[2]/div")[0].ChildNodes.Count;
+            print(1, 6, general_news, _doc, "Print Top Daily News");
+        }
+    }
+}
